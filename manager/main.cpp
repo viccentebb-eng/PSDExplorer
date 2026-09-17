88ff5bf192762642ada9286d42553b36a8f67bd7
viccentebb-eng <viccentebb@gmail.com>
2026-09-16 19:06:29 -0600

    import: add manager translation unit

---
+++ manager/main.cpp
@@ -0,0 +1,7 @@
+// PSD Explorer manager UI.
+// Split into include fragments only to keep the initial repository import manageable;
+// the compiler sees the same translation unit as the original 1.1.2 main.cpp.
+#include "main_part1.inc"
+#include "main_part2.inc"
+#include "main_part3.inc"
+#include "main_part4.inc"
